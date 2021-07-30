1b7b30bb170979015d14567b7cd76cf8227011c9
matheuseqc <65694801+matheuseqc@users.noreply.github.com>
2021-07-30 20:34:03 -0300

    Add files via upload

--- exibir-usu.php
+++ exibir-usu.php
@@ -1,3 +1,16 @@
+
+<html>
+
+    <body>
+        <form>
+            PROCURAR USUÁRIO: <input name="busca" type="text" placeholder="busca usuario" method="POST">
+            <a href="u.php"> 
+                <input type="button" value="enviar">
+            </a>
+        <form>
+        <br>
+    </body>
+</html>
 <?php
 include('conexao-bd.php');
 
@@ -17,6 +30,7 @@ if ($result->num_rows > 0){
         echo 'Usúario: ',$linha['usuario'];
         echo '<br>';
         echo '<hr>';
-    }
+    }  
 }
+
 ?>
